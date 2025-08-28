b1a6f7ced278ef16b331c1d5548c1a6bf4733973
Amay Mani Tripathi <amaytripathi19@gmail.com>
2025-08-28 23:42:27 +0530

    fix: build fix

--- lib/AuthProvider.tsx
+++ lib/AuthProvider.tsx
@@ -2,14 +2,14 @@
 
 import { SessionProvider } from "next-auth/react";
 import type { ReactNode } from "react";
-import type { Session } from "next-auth";
+
 
 export function Providers({
   children,
-  session,
+
 }: {
   children: ReactNode;
-  session?: Session | null;  // ✅ use Session type here, not SessionProvider
+ 
 }) {
-  return <SessionProvider session={session}>{children}</SessionProvider>;
+  return <SessionProvider>{children}</SessionProvider>;
 }
